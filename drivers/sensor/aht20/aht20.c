3c7c0c43b7f4ab3754b89afd64c2e1455d3df827
Rafael Lee <rafaellee.img@gmail.com>
2022-02-08 17:26:04 +0800

    sensors: remove aht20 header file
    Remove aht20.h to avoid unnecessary definitions.
    
    Signed-off-by: Rafael Lee <rafaellee.img@gmail.com>

--- drivers/sensor/aht20/aht20.c
+++ drivers/sensor/aht20/aht20.c
@@ -14,7 +14,38 @@
 #include <sys/crc.h>
 #include <sys/byteorder.h>
 
-#include "aht20.h"
+#define AHT20_STATUS_LENGTH 1
+#define AHT20_READ_LENGTH 6
+#define AHT20_CRC_POLY 0x31 // polynomial 1 + x^4 + x^5 + x^8
+#define AHT20_CRC_INIT 0xff
+
+#define AHT20_CMD_RESET 0xBA
+#define AHT20_CMD_TRIGGER_MEASURE 0xAC
+#define AHT20_TRIGGER_MEASURE_BYTE_0 0x33
+#define AHT20_TRIGGER_MEASURE_BYTE_1 0x00
+#define AHT20_CMD_GET_STATUS 0x71
+#define AHT20_CMD_INITIALIZE 0xBE
+
+#define AHT20_FULL_RANGE_BITS 20
+#define AHT20_TEMPERATURE_RANGE 200
+#define AHT20_TEMPERATURE_OFFSET 50.0
+
+typedef union {
+	struct {
+		uint8_t : 3;            // bit [0:2]
+		uint8_t cal_enable : 1; // bit [3]
+		uint8_t : 1;            // bit [4]
+		uint8_t : 2;            // bit [5:6], AHT20 datasheet v1.1 removed 2 mode bits
+		uint8_t busy : 1;       // bit [7]
+	};
+	uint8_t all;
+} __attribute__((__packed__)) aht20_status;
+
+struct aht20_data {
+	struct i2c_dt_spec bus;
+	uint32_t humidity;
+	uint32_t temperature;
+};
 
 LOG_MODULE_REGISTER(AHT20, CONFIG_SENSOR_LOG_LEVEL);
 
@@ -53,19 +84,18 @@ static int aht20_init(const struct device *dev)
 
 	static uint8_t const inquire_status_seq[] = { AHT20_CMD_GET_STATUS };
 
-	ret = i2c_write_dt(&drv_data->bus, inquire_status_seq, sizeof(inquire_status_seq));
+	ret = i2c_write_read_dt(&drv_data->bus, inquire_status_seq, sizeof(inquire_status_seq), &sensor_status.all, AHT20_STATUS_LENGTH);
 	if (0 != ret) {
 		LOG_ERR("Failed to inquire status");
 	}
 
-	ret = i2c_read_dt(&drv_data->bus, &sensor_status.all, AHT20_STATUS_LENGTH);
 	if (!sensor_status.cal_enable) {
 		static uint8_t const initialize_seq[] = { AHT20_CMD_INITIALIZE };
 		ret = i2c_write_dt(&drv_data->bus, initialize_seq, sizeof(initialize_seq));
 		if (0 != ret) {
 			LOG_ERR("Failed to inquire status");
 		}
-		k_busy_wait(10000); // wait for 10ms
+		k_sleep(K_MSEC(10));
 	}
 
 	return ret;
@@ -92,19 +122,19 @@ static int aht20_sample_fetch(const struct device *dev, enum sensor_channel chan
 
 	// tested with AHT20, 40ms is enough for measuring, datasheet said wait 80ms
 	k_sleep(K_MSEC(40));
-	bool wait = false;
 
-	do {
-		if (wait) {
-			k_sleep(K_MSEC(5)); // skip waiting for the first time, less kernel call involve
-		}
-		wait = true;
+	while (1) {
 		rc = i2c_read_dt(&drv_data->bus, rx_buf, AHT20_STATUS_LENGTH + AHT20_READ_LENGTH);
 		if (rc < 0) {
 			return -EIO;
 		}
 		sensor_status.all = rx_buf[0];
-	} while (sensor_status.busy);
+		if (sensor_status.busy) {
+			k_sleep(K_MSEC(5));
+		} else {
+			break;
+		}
+	}
 
 	// the read 7 bytes contains the following data:
 	// status: 8 bits
